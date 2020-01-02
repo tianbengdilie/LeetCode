9dbc4dfb79d9883de30c61ebdd8df0f67683d3eb
598010199 <598010199@qq.com>
2020-01-02 12:36:44 +0800

    完成329. Longest Increasing Path in a Matrix

--- 329. Longest Increasing Path in a Matrix/329. Longest Increasing Path in a Matrix.cpp
+++ 329. Longest Increasing Path in a Matrix/329. Longest Increasing Path in a Matrix.cpp
@@ -9,16 +9,23 @@ class Solution {
 	vector<vector<int>> memo;
 	int row, col;
 	int GetPathLen(const vector<vector<int>>& matrix, int x, int y) {
-		if (memo[x][y] != -1) return memo[x][y];
+		if (memo[x][y] != 0) return memo[x][y];
+		memo[x][y] = 1;
 		for (int i = 0; i < 4; i++) {
-
+			int nextX = x + dirX[i], nextY = y + dirY[i];
+			if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col)
+				continue;
+			if (matrix[nextX][nextY] > matrix[x][y]) {
+				memo[x][y] = max(memo[x][y], 1 + GetPathLen(matrix, nextX, nextY));
+			}
 		}
+		return memo[x][y];
 	}
 public:
 	int longestIncreasingPath(vector<vector<int>>& matrix) {
 		if (matrix.empty()) return 0;
 		row = matrix.size(), col = matrix.front().size();
-		memo = vector<vector<int>>(row, vector<int>(col, -1));
+		memo = vector<vector<int>>(row, vector<int>(col));
 		int ans = 0;
 		for (int x = 0; x < row; ++x) {
 			for (int y = 0; y < col; ++y) {
@@ -32,7 +39,10 @@ public:
 
 #ifdef _DEBUG
 int main() {
+	vector<vector<int>> matrix = { {1,2} };
 	Solution s;
+
+	cout << s.longestIncreasingPath(matrix) << endl;
 	return 0;
 }
 #endif
